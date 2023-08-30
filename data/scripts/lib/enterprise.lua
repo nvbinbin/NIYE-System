810d567b314e7fb0360cfe77ab1f32b95a15b3d1
nvbinbin <1661014215@qq.com>
2023-08-30 14:55:52 +0800

    2023年8月30日14:55:50

--- data/scripts/lib/enterprise.lua
+++ data/scripts/lib/enterprise.lua
@@ -36,7 +36,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 15, maxRandom = 100,
 
             text = {"欢迎使用系统升级插件 - 等级 X", "全银河30年联保  通讯:114514 - 1919"},
-            perfor = {{name = "先驱者", type = 0}, {name = "遗物", type = 0}}
+            perfor = {{name = "先驱者", type = 0}, {name = "遗物", type = 0}},
+            entry = {}
         },
         {
             uid = 1002, name = "索坦逆向科技", nameId = "Xsotan", rarity = 3, quality = 5, type = 1,
@@ -44,7 +45,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 15, maxRandom = 100,
 
             text = {"Xsotan Key 0X000000", "null - 000000"},
-            perfor = {{name = "逆向科技", type = 0}}
+            perfor = {{name = "逆向科技", type = 0}},
+            entry = {}
         },
 
         {
@@ -53,7 +55,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {"Xsotan System Decompile", "产自 - 闪耀科技研讨会"},
-            perfor = {{name = "逆向科技", type = 0}}
+            perfor = {{name = "逆向科技", type = 0}},
+            entry = {}
         },
         {
             uid = 0902, name = "暗金教会", nameId = "DGC", rarity = 2, quality = 5, type = 0,
@@ -61,7 +64,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {},
-            perfor = {{name = "暗金工程", type = 2}}
+            perfor = {{name = "暗金工程", type = 2}},
+            entry = {}
         },
 
         {
@@ -70,7 +74,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {},
-            perfor = {{name = "寥若晨星", type = 0}}
+            perfor = {{name = "寥若晨星", type = 0}},
+            entry = {}
         },
         {
             uid = 9902, name = "莱莎重工", nameId = "BA", rarity = 2, quality = 5, type = 1,
@@ -78,7 +83,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {},
-            perfor = {{name = "寥若晨星", type = 0}}
+            perfor = {{name = "寥若晨星", type = 0}},
+            entry = {}
         },
 
         {
@@ -87,7 +93,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {"通过最新的量子计算器", "我们已成功推算出传说之上"},
-            perfor = {}
+            perfor = {},
+            entry = {}
         },
         {
             uid = 0802, name = "天顶星集团", nameId = "HCK", rarity = 1, quality = 4, type = 0,
@@ -95,7 +102,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {"欢迎加入我们", "无论您是从事什么行业"},
-            perfor = {}
+            perfor = {},
+            entry = {}
         },
         {
             uid = 0803, name = "大秦军工", nameId = "QIN", rarity = 1, quality = 4, type = 1,
@@ -103,7 +111,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {"欢迎购买并使用我们的外贸产品"},
-            perfor = {}
+            perfor = {},
+            entry = {}
         },
         {
             uid = 0804, name = "惠民企业", nameId = "HM", rarity = 1, quality = 4, type = 2,
@@ -111,7 +120,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 10, maxRandom = 100,
 
             text = {"惠民企业给你带来更高端的产业升级"},
-            perfor = {}
+            perfor = {},
+            entry = {}
         }
 
 
@@ -147,7 +157,8 @@ function getEnterprise(seed, rarity, useType)
             minRandom = 0, maxRandom = 100,
 
             text = {},
-            perfor = {}
+            perfor = {},
+            entry = {}
         }
     end
 
@@ -199,12 +210,14 @@ function getLines(tech)
     local colors
     
     if next(tech.text) ~= nil then
+
         for i, v in pairs(tech.text) do
         table.insert(texts, {ltext = tech.text[i]%_t, lcolor = ColorRGB(1, 0.5, 0.6)})
         end
         table.insert(texts, {ltext = ""%_t})
     end
     if next(tech.perfor) ~= nil then
+
         table.insert(texts, {ltext = "厂牌特性："%_t, lcolor = ColorRGB(0.9, 0.5, 0.3)})
         for i, v in pairs(tech.perfor) do
             if tech.perfor[i].type == 0 then colors = ColorRGB(0.8, 0.8, 0.8) end
@@ -217,13 +230,68 @@ function getLines(tech)
     return texts
 end
     
-function getRoll()
+function getRoll(seed, tech)
+    math.randomseed(seed)
     --[[
-    概率声明：
-    10级卡片：5轮buff 5轮debuff 词条上限：5 正面buff概率：80 60 40 20 1  负面buff概率：50 20 10 5 5
-    9级卡片：  4                      
+        抽奖规则：卡片有 等级 - 3 次抽奖机会（红色有1次 紫色2次 8级3次 9级4次 10级5次）
+        默认情况下获得词条的概率为：30  20  10  1  0
+        厂牌等级每级会额外增加：4 的概率
+        如果是 超稀有 卡牌，都会额外增加 5 的概率
+        ----------
+        中奖后将有60%的概率是个好词条，30%的概率是个坏词条，10%的概率是个中性词条
+        厂牌等级的补正：  +3 -5 +2 
+        中奖的补正（每次中奖后）： -5 +3 +2                  
 
     ]]
+    local i = #tech.perfor
+    
+    local draw = tech.rarity - 3 --异域卡片开始便有一次抽卡机会
+    local drawProb = {30, 20, 10, 1, 0} -- 基础中奖概率
+    local goodEntry, badEntry, neutEntry = 60, 30, 10 -- 基础中奖类型
 
+    if draw > 2 then
+
+        local techDraw = draw - 2
+        for i, techDraw do--类型概率补正
+            for i, #drawProb do drawProb[i] = drawProb[i] + 4 end --基础概率补正
+            goodEntry = goodEntry + 3 
+            badEntry = badEntry -5
+            neutEntry = neutEntry + 2
+        end
+    end
+
+    if next(tech.perfor) ~= nil then
+
+        for i, v in pairs(tech.perfor) do
+            if tech.perfor[i].name == "寥若晨星" then
+                for i, #drawProb do drawProb[i] = drawProb[i] + 5 end --稀有补正
+            end
+        end
+    end
+
+    local quan = 0
+    local drawPerfor = {}
+
+    for i, draw do -- 原神抽卡
+        if math.random(0,100) < draw[quan + 1] then
+            quan = quan + 1
+        end
+    end
+
+    for i, quan do -- 抽出来啦！
+
+        local randoms = math.random(0,100)
+
+        if randoms <= goodEntry then
+            table.insert(tech.entry, {哇！金色传说！})
+        elseif randoms <= goodEntry + neutEntry then
+            table.insert(tech.entry, {哇！褒贬不一！})
+        else
+            table.insert(tech.entry, {好好好！})
+        end
+        goodEntry = goodEntry - 5 
+        badEntry = badEntry + 3
+        neutEntry = neutEntry + 2
+    end
 
 end
