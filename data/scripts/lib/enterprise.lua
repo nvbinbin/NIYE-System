8419dba4c959a4f68c4a4529ec89780725003105
nvbinbin <46121728+nvbinbin@users.noreply.github.com>
2023-12-15 14:37:25 +0800

    更新 enterprise.lua

--- data/scripts/lib/enterprise.lua
+++ data/scripts/lib/enterprise.lua
@@ -9,36 +9,74 @@ FixedEnergyRequirement = true
 
 --[[
 
-这里是企业函数库，使用include即可引用。
+这里是顶级企业的函数库，使用include引用对游戏进行修改。
 
 ]]
 
-------------------------------------------------------------------------------------------------------------
+----------------------------------------------
 -- 这段函数用于给暗金教会随机增加一个乱码描述XDD
---------------------------------------------------------------------------------------
+----------------------------------------------
+local chars = {
+    ["口"] = 2,
+    ["鍒"] = 0.5,
+    ["板"] = 0.5,
+    ["簳"] = 0.5,
+    ["浠"] = 0.5,
+    ["涔"] = 0.5,
+    ["堟"] = 0.5,
+    ["椂"] = 0.5,
+    ["鍊"] = 0.5,
+    ["椤"] = 0.5,
+    ["紑"] = 0.5,
+    ["03"] = 0.5,
+    ["15"] = 0.5,
+    ["16"] = 0.5,
+    ["18"] = 0.5,
+    ["20"] = 0.5,
+    ["05"] = 0.5,
+    ["24"] = 0.5,
+    ["??"] = 0.5,
+    ["锟"] = 0.5,
+    ["斤"] = 0.5,
+    ["拷"] = 0.5
+}
+-- 获取权重信息
+function getMaxWeight(t)
+    local wt = {}
+    -- 初始化权重
+    local sum = 0
+    for k, v in pairs (t) do
+        sum = sum + v -- 累计权重
+        wt [sum] = k -- 以权重总和为键，元素为值，存入新表
+    end
+    wt.max = sum -- 记录权重
+    return wt
+end
 
+-- 打印机
 function makeText(seed,num)
     math.randomseed(seed)
-    -- 暗金教会正在施法
-    local chars = {"天","地","玄","黄","宇","宙","洪","荒","金","木","水","火","土","乾","坤","震","艮","离","坎","兑","巽","口","口","口","口","口","口","口","锟","斤","拷","锟","斤","拷","锟","斤","拷","锟","斤","拷","锟","斤","拷","锟","斤","拷","锟","斤","拷","口","口","口","口","口","口"}
-    local result = ""
-  for i = 1, num do
-    local index = math.random(1,#chars)
-    local char = chars[index]
-    result = result .. char
-  end
-  return result
+    local wt = getMaxWeight(chars)
+    -- 抽取字符
+    local result = {}
+    for i = 1, num do
+        local r = math.random (wt.max)
+        for k, v in pairs (wt) do
+            if r <= k then table.insert(result, v) end
+        end
+    end
+    -- 返回连接后的字符串
+    return table.concat(result)
 end
 
 function churchText(seed, tech)
     math.randomseed(seed)
     -- 暗金教会开始施法
-    local str = ""
-    str = str .. makeSerialNumber(seed, 2, nil, "-", "QWERTYUIOPASDFGHJKLZXCVBNM")
-    str = str .. makeSerialNumber(seed, 3, nil, "-", "1234567890")
-    str = str .. makeText(seed, 6)
-    table.insert(tech.text, str)
-
+    local wt = {}
+    table.insert(wt, makeSerialNumber(seed, 2, nil, "-", "QWERTYUIOPASDFGHJKLZXCVBNM"))
+    table.insert(wt, makeSerialNumber(seed, 3, nil, "-", "1234567890"))
+    table.insert(wt, makeText(seed, 6))
+    table.insert(tech.text, table.concat(wt))
 end
 -- 传入表单会增加性能开销
 function churchTip(texts, bonuses, ltext, rtext, icon, permanent)
@@ -49,10 +87,9 @@ function churchTip(texts, bonuses, ltext, rtext, icon, permanent)
     end
     return texts, bonuses
 end
+-----------------------------
 
-
--------------------------------------------------------------------------------------------------------
--- 批量送出图标
+-- 企业为所有系统应用上新图标
 function makeIcon(name, tech)
     local icon = "data/textures/icons/" .. name
     if tech.uid == 0700 then
@@ -63,75 +100,98 @@ function makeIcon(name, tech)
     return icon
 end
 
+--[[企业类型
+    militaryEnterprise 军工企业ME：专注于军用的装备系统研发生产企业
+    civilEnterprise 民工企业CE：专注于民用的装备系统研发生产企业
+    hybridEnterprise 混合企业HE:军用和民用都有涉猎的混合生产企业
 
---[[
-                                    奖池系统已被移除
-function addJackpot(tech, tb)
+    军用系统：武装栏位
+    民用系统：民用栏位；充能电池；能量电池
+    独立系统：自动栏位；通用栏位；能量护盾；
+    军用武器：
+    民用武器：
 
-    for k, v in pairs(tb) do
-        -- 数值合并
-        if tech[k] and type(v) == "number" and type(tech[k]) == "number" then
-            tech[k] = tech[k] + v
-        -- 表单合并
-        elseif tech[k] and type(v) == "table" and type(tech[k]) == "table" then
-            tech[k] = (tech[k] or {}) .. v
-        end
-    end
+    ]]
+--[[企业规模：A+B+C
+    A:企业的规模大小决定了这个企业铺设了多少的生产线作用于不同的装备系统。
+        1:small 小型：推出一件改良
+        2:medium 中型：推出两件改良
+        3:large 大型：推出三件改良
+        4:top 顶级：推出四件改良
+        5:special 特殊：似乎什么改良都有涉及
+    
+    B:企业的生产类型决定了是推出装备还是系统升级。
+        1.equipment 装备：只会推出炮台
+        2.technology 科技：只会推出系统
+        3.general 通用：炮台和系统都有
 
-end
-]]
+    C:企业的品控决定了出现的概率，额外的属性和其他参数。
+        1.盈利目的型活动部门；此类部门主要目的是为了利益性赚钱。他们拥有比较稳定的品控和售价，出现的概率也比较高。
+        常规名称：企业；公司；集团
+        价格：口口
+        质量：口口
+        概率：口口口口
+        基础出现概率：MKI[0.06] MKII[0.03] MKIII[0.006]
+        基础价格倍率：110%
+        基础能量倍率：120%
+        基础生产范围：红色
+
+        2.科研目的的活动部门；此类部门主要是为了更高技术的装备系统生产。他们拥有非常稳定的品控，但是售价和出现的概率都比较高。
+        常规名称：研究所；科研所；
+        价格：口口口口
+        质量：口口口
+        概率：口口
+        基础出现概率：MKI[0.03] MKII[0.015] MKIII[0.003]
+        基础价格倍率：125%
+        基础能量倍率：110%
+        基础生产范围：紫色
+
+        3.这是以定制的化为主活动部门，他们往往只生产高品质产品，这就导致了此类物品在市场上的流通极为罕见。
+        常规名称：工程部，会议，彩蛋
+        价格：口口口口口
+        质量：口口口口
+        概率：口
+        基础出现概率：MKI[0.01] MKII[0.005] MKIII[0.001]
+        基础价格倍率：145%
+        基础能量倍率：100%
+        基础生产范围：紫色
+
+        4.暗金教会，这是暗金教会的东西，因为很少流通并且数值也被加密，所以很难定价。
+        常规名称：教会
+        价格：口
+        质量：口
+        概率：口
+        基础出现概率：MKI[0.02] MKII[0.01] MKIII[0.002]
+        基础价格倍率：50%
+        基础能量倍率：110%
+        基础生产范围：紫色
+
+    ]]
 
 function getEnterprise(seed, rarity, useType)
     math.randomseed(seed)
     
-    --[[企业类型：military;civil;current
-        军工企业：这个企业专注于战斗的武器和系统插件研发生产
-        民工企业：这个企业专注于民用的武器和系统插件研发生产
-        混合：这个企业什么都能生产
+    
+    local enters ={}
 
-        军用系统：武装栏位
-        民用系统：民用栏位
-        独立系统：自动栏位；通用栏位；能量护盾；
-
-    ]]
-    --[[企业规模：A+B+C
-        小型：推出一件改良
-        中型：推出两件改良
-        大型：推出三件改良
-        顶级：推出四件改良
-        特殊：似乎什么改良都有涉及
-
-        军火：只会推出炮台
-        科技：只会推出系统
-        通用：炮台和系统都有
-
-        企业/集团：这是一个以生产贸易为主要的活动部门
-        价格：口口口
-        质量：口口
-        概率：口口口口
-
-        研究所/研究会：这是一个以研发和创造改进为主要的活动部门
-        价格：口口口口
-        质量：口口口口
-        概率：口口口
-
-        会议/教会：这是一个特殊部门，他们往往不会流通在市场
-        价格：口口口
-        质量：口口
-        概率：口
-
-        不使用C：这是一个高级品牌，准确来说算奢侈品
-        价格：口口口口口
-        质量：口口口口口
-        概率：口
-
-
-    ]]
-    local entes ={}
     local ent = {}
-    ent.uid = 1001  ent.name = "大秦军工"%_t  ent.nameId = "QIN"  ent.enteType = "current"%_t  ent.enteScale = 3  ent.enteWork = 3  ent.enteStru = {1, "企业"}
-            --是否系统插件  产品ID
-    ent.put1 = {false, }
+    ent.uid = 1001  ent.name = "Daqin Military/*大秦军工*/"%_t  ent.nameId = "QIN"  ent.enterType = "HybridEnterprise/*混合企业*/"%_t
+    -- 企业规模         企业业务范围         企业品控               企业后缀
+    ent.enterScale = 3  ent.enterWork = 3  ent.enterQuality = 1  ent.enterSuffix = "Enterprise/*企业*/"
+    --是否系统插件  产品ID   最小等级  最大等级
+    ent.put = {
+        {true, "militarytcs", 1, 2},
+        {false, "RailGun", 1, 3},
+        {false, "ChainGun", 1, 2}
+    }
+    ent.tipLink = 2
+    ent.tip_1 = {"欢迎购买并使用大秦军工的产品"%_t}
+    ent.tip_2 = {"我还没想好这里写什么"%_t}
+    ent.perforLink = 1
+    ent.perfor = {{name = "稳定品控"%_t, tip = "最低品质+10%", type =1}}
+    
+    table.insert(enters, ent)
+
 
 
     local entes = {
